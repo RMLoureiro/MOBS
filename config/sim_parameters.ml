66c56a282695be2d97983965fa6b759a848d7699
Miguel Alves <42934491+mce-alves@users.noreply.github.com>
2021-05-20 18:13:22 +0100

    Added the ability to define parameters in JSON and parse them
    This will be taken advantage of when the parameters are defined through an UI, this way it can produce the JSON parameter file immediately

---
+++ config/sim_parameters.ml
@@ -0,0 +1,20 @@
+(* this file will contain the simulation parameters *)
+(* reads from JSON file so that changing parameters doesn't require recompiling the code *)
+
+let parameters_file = "parameters.json"
+
+(* parameters and default values *)
+let num_nodes = ref 10
+let end_block_height = ref 5
+
+
+(* TODO : add other parameters as they are needed *)
+
+
+let () =
+  let json = Yojson.Basic.from_file parameters_file in
+  let open Yojson.Basic.Util in
+  num_nodes := json |> member "num-nodes" |> to_int;
+  end_block_height := json |> member "end-block-height" |> to_int
+
+
