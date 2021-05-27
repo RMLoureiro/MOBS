2e130db84761a9856efa48b6b3d1a7458d7eaf98
Miguel Alves <42934491+mce-alves@users.noreply.github.com>
2021-05-27 13:02:52 +0100

    Added simple network debug test

---
+++ test/network/network_test.ml
@@ -0,0 +1,9 @@
+let () = 
+  let regions = Abstractions.Network.node_regions in
+  let links   = Abstractions.Network.node_links in
+  print_string "Node Regions Assigned: [";
+  List.iter (fun x -> print_int x; print_string ", ") regions;
+  print_endline "]";
+  print_endline "Node Links Assigned: [";
+  List.iter (fun x -> print_string "["; (List.iter (fun y -> print_int y; print_string ", ") x); print_endline "],") links;
+  print_endline "]"
