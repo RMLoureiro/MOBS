e55c29875180767ddb138e649aa6177cff0c0b2e
Miguel Alves <42934491+mce-alves@users.noreply.github.com>
2021-06-17 12:20:37 +0100

    Added a print to keep track of the elapsed time of the simulation

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,7 @@
 
 let () = 
-  print_endline "Starting simulation";
+  let t = Sys.time () in
+  print_endline "\t Running simulation...";
   Implementation.Algorand.AlgorandProtocol.run ();
-  print_endline "Simulation completed"
-
+  let elapsed_time = Sys.time () -. t in
+  Printf.printf "\t Simulation completed in %.2f seconds. \n" elapsed_time
