db564a69b2f77a368f7de1b3f126389237aad45e
Ricardo Loureiro <loureiro@Ricardos-MBP.Home>
2024-01-12 01:03:17 +0000

    bump

--- scripts/paxos.py
+++ scripts/paxos.py
@@ -8,7 +8,6 @@ def read_json_file(file_path):
             json_array = json.load(file)
             for json_object in json_array:
                 read_json_object(json_object)
-            print(results)
             for key in results:
                 if key != None:
                     print('Value: ' + str(key) + ' Reached at: ' + str(results[key]["timestamp"]) + 'ms nodes agreed before: ' + str(len(results[key]["agreed_before"])) + ' nodes agreed after:' + str(len(results[key]["agreed_after"])))
@@ -29,7 +28,9 @@ def read_json_object(json_object):
             if results[value]["timestamp"] == -1:
                 results[value]["agreed_before"].add(json_object.get('content').get('begin-node-id'))
             else:
-                results[value]["agreed_after"].add(json_object.get('content').get('begin-node-id'))
+                node_id = json_object.get('content').get('begin-node-id')
+                if not node_id in results[value]["agreed_before"]:
+                    results[value]["agreed_after"].add(node_id)
                 
             
 def initialize_value(value):
